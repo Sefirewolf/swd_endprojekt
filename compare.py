91c4d923e71507a432bf7d5b2aec565bc25b416f
Sefirewolf <hp4021@mci4me.at>
2024-03-01 03:29:42 +0100

    datenbank muss angepasst werden um compare funktion zu unterstützen

--- compare.py
+++ compare.py
@@ -1,7 +1,34 @@
-import streamlit as st
-import librosa
-import librosa.display
 import numpy as np
-import matplotlib.pyplot as plt
-import convert_mp3 
+import convert_mp3 as cmp3
+import database
+
+def score_match(offsets):
+
+    differences = [offset[0] - offset[1] for offset in offsets]
+    hist, _ = np.histogram(differences, bins='auto')
+
+    return np.max(hist)
+
+
+
+def best_match(matches):
+    matched_song = None
+    top_score = 0
+
+    for song_id, offsets in matches.items(): 
+        score = score_match(offsets)
+        if score > top_score:
+            top_score = score
+            matched_song = song_id
+    
+    return matched_song
+
+
+
+def compare_files(audio_file):
+    #Cmpares audiofile to database
+
+    hash = cmp3.generate_hash(audio_file)
+    match = get_maches(hash)#funktion in datenbank um alle hashes zu erlangen
+    result = best_match(match)
 
